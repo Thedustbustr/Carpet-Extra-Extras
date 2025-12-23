de11501c79d34952b71aeffc8d9ec13d2aa555f1
Thedustbuster <92692948+Thedustbustr@users.noreply.github.com>
2025-12-23 01:08:32 -0500

    release(1.21.11-1.2.7) (#22)
    * fix:(lithium 0.21.x compatibility)
    
    * fix:(runtime lithium 0.21.x compatibility)

--- src/main/java/net/thedustbuster/mixin/compatibility/LithiumHopperHelperMixin.java
+++ src/main/java/net/thedustbuster/mixin/compatibility/LithiumHopperHelperMixin.java
@@ -1,8 +1,7 @@
 package net.thedustbuster.mixin.compatibility;
 
-import net.minecraft.core.Direction;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.Container;
-import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.item.ItemStack;
 import net.thedustbuster.rules.ShulkerBoxStackLimit;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,13 +14,13 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class LithiumHopperHelperMixin {
   @SuppressWarnings("UnresolvedMixinReference")
   @Redirect(
-    method = "Lnet/caffeinemc/mods/lithium/common/hopper/HopperHelper;tryMoveSingleItem(Lnet/minecraft/world/Container;Lnet/minecraft/world/WorldlyContainer;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;ILnet/minecraft/core/Direction;)Z",
+    method = "tryMoveSingleItem(Lnet/minecraft/world/Container;Lnet/minecraft/world/WorldlyContainer;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;ILnet/minecraft/core/Direction;)Z",
     at = @At(
       value = "INVOKE",
       target = "Lnet/minecraft/world/item/ItemStack;isSameItemSameComponents(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"
     )
   )
-  private static boolean customMergeCheck(ItemStack stack1, ItemStack stack2, Container to, WorldlyContainer toSided, ItemStack transferStack, ItemStack transferChecker, int targetSlot, Direction fromDirection) {
-    return ShulkerBoxStackLimit.canMergeItems(stack1, stack2, to).getOrElse(true) && ItemStack.isSameItemSameComponents(stack1, stack2);
+  private static boolean customMergeCheck(ItemStack stack1, ItemStack stack2, @Local(argsOnly = true) Container container) {
+    return ShulkerBoxStackLimit.canMergeItems(stack1, stack2, container).getOrElse(true) && ItemStack.isSameItemSameComponents(stack1, stack2);
   }
 }
