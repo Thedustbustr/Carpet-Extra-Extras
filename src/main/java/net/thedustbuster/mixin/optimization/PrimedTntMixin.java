75f096126f13ea5d96eb6c6c0fdfe9f330207d1f
Thedustbuster <92692948+Thedustbustr@users.noreply.github.com>
2025-12-31 11:32:02 -0500

    release(1.21.11-1.2.8) (#24)
    * fix(optimizedTNTInteraction breaking contraptions relying on lazy tnt acceleration); feat(optimizedTNTInteraction is now marked as EXPERIMENTAL)
    
    * fix:(version bump)

---
+++ src/main/java/net/thedustbuster/mixin/optimization/PrimedTntMixin.java
@@ -0,0 +1,39 @@
+package net.thedustbuster.mixin.optimization;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.PrimedTnt;
+import net.minecraft.world.level.Level;
+import net.thedustbuster.adaptors.minecraft.worldgen.ChunkHelper;
+import net.thedustbuster.adaptors.minecraft.worldgen.ChunkLevel;
+import net.thedustbuster.tags.LazyTntTag;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PrimedTnt.class)
+public abstract class PrimedTntMixin implements LazyTntTag {
+  @Unique
+  private boolean cee$spawnedInLazyChunk;
+
+  @Override
+  public boolean cee$spawnedInLazyChunk() {
+    return this.cee$spawnedInLazyChunk;
+  }
+
+  @Inject(
+    method = "<init>(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/entity/LivingEntity;)V",
+    at = @At("TAIL")
+  )
+  private void cee$detectLazyChunk(Level level, double x, double y, double z, LivingEntity livingEntity, CallbackInfo ci) {
+    if (level instanceof ServerLevel lvl) {
+      // Convert to chunk coords
+      int cx = ((int) x) >> 4;
+      int cz = ((int) z) >> 4;
+
+      this.cee$spawnedInLazyChunk = ChunkHelper.isChunkAtLevel(lvl, ChunkLevel.LAZY, cx, cz);
+    }
+  }
+}
